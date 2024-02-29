325bcde2a0d220f3a543bb45ca0e9f910d952ed5
FRVR-JAE <tjoloyede9@gmail.com>
2024-02-29 12:04:18 -0800

    1

--- eee/src/Components/About.tsx
+++ eee/src/Components/About.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import "../styles/about.css"
-import ImageSlider from './ImageSlider' // import the ImageSlider component
+import React from "react";
+import "../styles/about.css";
+import ImageSlider from "./ImageSlider"; // import the ImageSlider component
 
 const About = () => {
   return (
@@ -9,8 +9,21 @@ const About = () => {
         <div className="row">
           <div className="about-text">
             <h2>Our Story</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eleifend lacus ut massa efficitur, eget maximus ligula finibus.</p>
-            <button className="contact-us-btn navlink-hover">Contact Us</button>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+              eleifend lacus ut massa efficitur, eget maximus ligula finibus.
+            </p>
+            <button className="contact-us-btn navlink-hover">
+              {" "}
+              <a
+                href="https://wa.me/+18322489686"
+                className="no-decor"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Contact us
+              </a>
+            </button>
           </div>
           <div className="image-slider">
             <ImageSlider />
@@ -19,6 +32,6 @@ const About = () => {
       </div>
     </section>
   );
-}
+};
 
 export default About;
--- eee/src/Components/Navbar.tsx
+++ eee/src/Components/Navbar.tsx
@@ -1,26 +1,77 @@
-import React, { useState, useEffect } from 'react';
-import { Link, Element } from 'react-scroll';
-import '../styles/navbar.css';
-import { FiMenu, FiX } from 'react-icons/fi';
+import React, { useState, useEffect } from "react";
+import { Link, Element } from "react-scroll";
+import "../styles/navbar.css";
+import { FiMenu, FiX } from "react-icons/fi";
 
 const Navbar: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+
+  useEffect(() => {
+    const closeSidebarOnOutsideClick = (event: MouseEvent) => {
+      if (isMenuOpen) {
+        const target = event.target as HTMLElement;
+        if (!target.closest('.navbar-menu') && !target.closest('.navbar-toggle')) {
+          setIsMenuOpen(false);
+        }
+      }
+    };
+
+    document.body.addEventListener('click', closeSidebarOnOutsideClick);
+
+    return () => {
+      document.body.removeEventListener('click', closeSidebarOnOutsideClick);
+    };
+  }, [isMenuOpen]);
+
   const NavLinks = () => (
     <>
-    <Link activeClass="active" to="home" spy={true} smooth={true} duration={500} className="navbar-item">
+      <Link
+        activeClass="active"
+        to="home"
+        spy={true}
+        smooth={true}
+        duration={500}
+        className="navbar-item"
+      >
         Home
       </Link>
-      <Link activeClass="active" to="about" spy={true} smooth={true} duration={500} className="navbar-item">
+      <Link
+        activeClass="active"
+        to="about"
+        spy={true}
+        smooth={true}
+        duration={500}
+        className="navbar-item"
+      >
         About
       </Link>
-      <Link activeClass="active" to="services" spy={true} smooth={true} duration={500} className="navbar-item">
+      <Link
+        activeClass="active"
+        to="services"
+        spy={true}
+        smooth={true}
+        duration={500}
+        className="navbar-item"
+      >
         Services
       </Link>
-      <Link activeClass="active" to="testimonials" spy={true} smooth={true} duration={500} className="navbar-item">
+      <Link
+        activeClass="active"
+        to="testimonials"
+        spy={true}
+        smooth={true}
+        duration={500}
+        className="navbar-item"
+      >
         Testimonials
       </Link>
-      <a href='' className="navbar-item">
+      <a
+        href="https://wa.me/+18322489686"
+        className="navbar-item"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         Contact
       </a>
     </>
@@ -33,20 +84,20 @@ const Navbar: React.FC = () => {
   return (
     <nav className={`navbar`}>
       <div className="navbar-container">
-      <a className="navbar-logo animate-e">
-  Triple-E
-  <span className="e">E</span>
-  <span className="e">E</span>
-</a>
+        <a className="navbar-logo animate-e">
+          Triple-E
+          <span className="e">E</span>
+          <span className="e">E</span>
+        </a>
 
-
-        
-        <div className={`navbar-toggle`} onClick={() => setIsMenuOpen(!isMenuOpen)}>
-          {!isMenuOpen && <FiMenu size={24} />}
-          
+        <div
+          className={`navbar-toggle`}
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          {!isMenuOpen ? <FiMenu size={24}  /> : <FiMenu size={24} />}
         </div>
 
-        <div className={`navbar-menu ${isMenuOpen ? 'active' : ''}`}>
+        <div className={`navbar-menu ${isMenuOpen ? "active" : ""}`}>
           <NavLinks />
 
           <div className="sidebar-close" onClick={handleCloseSidebar}>
@@ -54,9 +105,9 @@ const Navbar: React.FC = () => {
           </div>
         </div>
       </div>
-      
     </nav>
   );
+  
 };
 
 export default Navbar;
