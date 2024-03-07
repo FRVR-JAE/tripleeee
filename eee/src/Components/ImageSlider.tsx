ba3f0deb6fcea160ac3386b3151cc1899df0fe57
FRVR-JAE <tjoloyede9@gmail.com>
2024-03-06 20:42:33 -0800

    1

--- eee/src/Components/ImageSlider.tsx
+++ eee/src/Components/ImageSlider.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { Component } from 'react';
 
 interface ImageSliderProps {}
 
@@ -10,21 +10,31 @@ interface ImageRef {
   current: HTMLImageElement | null;
 }
 
-class ImageSlider extends React.Component<ImageSliderProps, ImageSliderState> {
+class ImageSlider extends Component<ImageSliderProps, ImageSliderState> {
   imageRefs: React.RefObject<HTMLImageElement>[];
+  intervalId: NodeJS.Timeout | null;
 
   constructor(props: ImageSliderProps) {
     super(props);
 
     this.imageRefs = Array.from({ length: 2 }).map((_, i) => React.createRef<HTMLImageElement>());
+    this.intervalId = null;
 
     this.state = {
       currentIndex: 0,
     };
+  }
 
-    setInterval(() => {
+  componentDidMount() {
+    this.intervalId = setInterval(() => {
       this.showNext();
     }, 5000);
+  }
+
+  componentWillUnmount() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
   }
 
   showNext() {
@@ -45,8 +55,16 @@ class ImageSlider extends React.Component<ImageSliderProps, ImageSliderState> {
 
     return (
       <div className="image-slider">
-        <img ref={imageRefs[0]} src="https://images.unsplash.com/photo-1623475173140-ad2f0369ca92?q=80&w=1924&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="active" />
-        <img ref={imageRefs[1]} src="https://images.unsplash.com/photo-1555244162-803834f70033?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+        <img
+          ref={imageRefs[0]}
+          src="https://images.unsplash.com/photo-1623475173140-ad2f0369ca92?q=80&w=1924&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          className={this.state.currentIndex === 0 ? 'active' : ''}
+        />
+        <img
+          ref={imageRefs[1]}
+          src="https://images.unsplash.com/photo-1555244162-803834f70033?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          className={this.state.currentIndex === 1 ? 'active' : ''}
+        />
       </div>
     );
   }
